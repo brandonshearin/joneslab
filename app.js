de1bd839c57efdb59235c858643996884943419f
brandon <bshearin15@gmail.com>
2017-08-09 02:53:48 +0000

    landing page done, not connected to any javascript

--- app.js
+++ app.js
@@ -3,3 +3,22 @@ var express = require('express'),
     
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/views/public/'));
+
+
+
+app.get('/', (req, res)=>{
+    res.render('home'); 
+});
+
+
+
+
+
+
+
+
+
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log('servering up the labb');
+});
